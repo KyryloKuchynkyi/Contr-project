249f2414d53b3f12162f09f8ed01381b86e4fa10
Kyrylo <kyrylokuchynskyi@gmail.com>
2021-01-04 20:34:13 +0200

    changed js in project

--- homeApp.js
+++ homeApp.js
@@ -1,43 +1,38 @@
-$(document).ready(function () {
+// MOBILE MENU
+
+let mobileMenu = document.querySelector('.nav-gamburger-menu');
+let mainMenu = document.querySelector(".navigation");
 
 
-    // MOBILE MENU
-
-    let mobileMenu = document.querySelector('.nav-gamburger-menu');
-    let mainMenu = document.querySelector(".navigation");
-
-
-    mobileMenu.addEventListener('click', function () {
-        mobileMenu.classList.toggle("active-menu");
-        if (mobileMenu.classList.contains("active-menu")) {
-            mainMenu.classList.add("active-menu");
-        } else {
-            mainMenu.classList.remove("active-menu");
-        }
-    })
-
-    // CLOSE MOBILE MENU
-
-    let closeM = document.querySelector('.close')
-
-
-    function closeModal() {
-        mainMenu.classList.remove("active-menu")
-
+mobileMenu.addEventListener('click', function () {
+    mobileMenu.classList.toggle("active-menu");
+    if (mobileMenu.classList.contains("active-menu")) {
+        mainMenu.classList.add("active-menu");
+    } else {
+        mainMenu.classList.remove("active-menu");
     }
-
-    closeM.addEventListener("click", closeModal)
-
-
-
-
-    // SLIDER
-
-
-    $('.slider-block').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
-    });
-    
 })
+
+// CLOSE MOBILE MENU
+
+let closeM = document.querySelector('.close')
+
+
+function closeModal() {
+    mainMenu.classList.remove("active-menu")
+
+}
+
+closeM.addEventListener("click", closeModal)
+
+
+
+
+// SLIDER
+
+
+$('.slider-block').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+});

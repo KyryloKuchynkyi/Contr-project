7189957bc36f34e85f46fd7d467ca72b2f10b1c7
Kyrylo <kyrylokuchynskyi@gmail.com>
2021-01-04 19:59:00 +0200

    added slider to second page

--- homeApp.js
+++ homeApp.js
@@ -1,28 +1,43 @@
+$(document).ready(function () {
 
 
-// MOBILE MENU
+    // MOBILE MENU
 
-let mobileMenu = document.querySelector('.nav-gamburger-menu');
-let mainMenu = document.querySelector(".navigation");
+    let mobileMenu = document.querySelector('.nav-gamburger-menu');
+    let mainMenu = document.querySelector(".navigation");
 
 
-mobileMenu.addEventListener('click', function() {
-    mobileMenu.classList.toggle("active-menu");
-    if(mobileMenu.classList.contains("active-menu")) {
-        mainMenu.classList.add("active-menu");
-    } else {
-        mainMenu.classList.remove("active-menu");
+    mobileMenu.addEventListener('click', function () {
+        mobileMenu.classList.toggle("active-menu");
+        if (mobileMenu.classList.contains("active-menu")) {
+            mainMenu.classList.add("active-menu");
+        } else {
+            mainMenu.classList.remove("active-menu");
+        }
+    })
+
+    // CLOSE MOBILE MENU
+
+    let closeM = document.querySelector('.close')
+
+
+    function closeModal() {
+        mainMenu.classList.remove("active-menu")
+
     }
-})
 
-// CLOSE MOBILE MENU
-
-let closeM = document.querySelector('.close')
+    closeM.addEventListener("click", closeModal)
 
 
-function closeModal() {
-    mainMenu.classList.remove("active-menu")
+
+
+    // SLIDER
+
+
+    $('.slider-block').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
     
-}
-
-closeM.addEventListener("click",closeModal)
+})
